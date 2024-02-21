0870536d4fb84f9dd434d6279adb953a08a9e15b
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-02-22 00:28:08 +0530

    some ui fix

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import logo from "../../images/logo2.png";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { FaUser, FaShoppingCart, FaRegUser } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
@@ -34,14 +33,17 @@ function Navbar() {
 
   return (
     <nav className="w-full grid py-5 px-10 bg-[#131921ff]">
-      <nav className="grid sm:grid-cols-12 justify-between items-center text-white">
-        <div id="sidebar" className="col-span-2 sm:col-span-4 ">
+      <nav className="grid grid-cols-12 justify-between items-center text-white">
+        <div id="sidebar" className="col-span-3 sm:col-span-4 ">
           <GiHamburgerMenu
-            className="col-span-3 text-2xl sm:text-3xl hover:scale-125 sm:hover:scale-110 duration-200 ease-linear "
+            className="sm:col-span-3 text-2xl sm:text-3xl hover:scale-125 sm:hover:scale-110 duration-200 ease-linear "
             onClick={toggleSidebar}
           />
         </div>
-        <div id="logo" className=" col-span-4 grid items-center justify-center">
+        <div
+          id="logo"
+          className="col-span-6 sm:col-span-4 grid items-center justify-center"
+        >
           <Link
             to={"/"}
             className="px-5 py-1 rounded-xl text-white font-bold text-xl shadow-sm shadow-gray-200 max-w-fit"
@@ -51,7 +53,7 @@ function Navbar() {
         </div>
         <div
           id="cart_auth"
-          className="col-span-2 sm:col-span-4 flex items-center justify-end gap-8"
+          className="col-span-3 sm:col-span-4 flex items-center justify-end gap-8"
         >
           {!user ? (
             <Link
@@ -131,7 +133,7 @@ function Navbar() {
               <Link
                 to="/"
                 onClick={() => dispatch(logoutUserAsync())}
-                className="flex font-semibold px-4 py-1 border border-white hover:bg-white hover:text-[#131921ff] ease-in-out duration-200"
+                className="flex items-center justify-center font-semibold px-4 py-1 border bg-[#131921ff] text-white hover:bg-transparent hover:border-[#131921ff] hover:text-[#131921ff] ease-in-out duration-200"
               >
                 Logout
               </Link>
